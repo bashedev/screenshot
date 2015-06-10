23a5c5a4f138533f49c315e8a754725b89d06a32
Joe Bashe <joba@localhost.localdomain>
2015-06-10 14:00:45 +0200

    initial repo creation

---
+++ screenshot.js
@@ -0,0 +1,71 @@
+// Render Multiple URLs to file
+
+var RenderUrlsToFile, arrayOfUrls, system;
+
+system = require("system");
+var fs = require('fs');
+
+/*
+Render given urls
+@param array of URLs to render
+@param callbackPerUrl Function called after finishing each URL, including the last URL
+@param callbackFinal Function called after finishing everything
+*/
+RenderUrlsToFile = function(urls, callbackPerUrl, callbackFinal) {
+    var getFilename, next, page, retrieve, webpage;
+    webpage = require("webpage");
+    page = null;
+    getFilename = function(url) {
+        return url.substring(7, url.search(/\./)) + '.png';
+    };
+    next = function(status, url, file) {
+        page.close();
+        callbackPerUrl(status, url, file);
+        return retrieve();
+    };
+    retrieve = function() {
+        var url;
+        if (urls.length > 0) {
+            url = urls.shift();
+            page = webpage.create();
+            page.viewportSize = {
+                width: 800,
+                height: 600
+            };
+            page.settings.userAgent = "Phantom.js bot";
+            return page.open(url, function(status) {
+                var file;
+                file = getFilename(url);
+                if (status === "success") {
+                    return window.setTimeout((function() {
+                        page.render(file);
+                        return next(status, url, file);
+                    }), 200);
+                } else {
+                    return next(status, url, file);
+                }
+            });
+        } else {
+            return callbackFinal();
+        }
+    };
+    return retrieve();
+};
+
+arrayOfUrls = null;
+
+if (system.args.length > 1) {
+    arrayOfUrls = Array.prototype.slice.call(system.args, 1);
+} else {
+    arrayOfUrls = fs.read('urls.txt').toString().split("\n");
+}
+
+RenderUrlsToFile(arrayOfUrls, (function(status, url, file) {
+    if (status !== "success") {
+        return console.log("Unable to render '" + url + "'");
+    } else {
+        return console.log("Rendered '" + url + "' at '" + file + "'");
+    }
+}), function() {
+    return phantom.exit();
+});
